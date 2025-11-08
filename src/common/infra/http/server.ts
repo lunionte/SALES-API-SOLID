8452a666f07f74998b7d76ff876be7bbfb5749ea
Italo <italoteles399@gmail.com>
2025-11-08 12:13:45 -0300

    alterado env de PORT para a validação do zod

--- src/common/infra/http/server.ts
+++ src/common/infra/http/server.ts
@@ -1,8 +1,9 @@
 import dotenv from "dotenv";
 dotenv.config();
 import { app } from "./app";
+import { env } from "../env";
 
-const PORT = process.env.PORT;
+const PORT = env.PORT;
 app.listen(PORT, () => {
     console.log("Servidor rodando na porta", PORT);
 });
