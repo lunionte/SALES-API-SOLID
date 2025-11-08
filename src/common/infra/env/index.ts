543ffac2fefbcf0233c7b8ef4b500eb13565fb5f
Italo <italoteles399@gmail.com>
2025-11-08 12:22:19 -0300

    alterado validações para mais controle e restrição nas variaveis de ambiente

--- src/common/infra/env/index.ts
+++ src/common/infra/env/index.ts
@@ -1,17 +1,17 @@
 import dotenv from "dotenv";
 import z from "zod";
-import { AppError } from "../../domain/errors/app.error";
 
 dotenv.config();
 const envSchema = z.object({
-    NODE_ENV: z.enum(["development", "production", "test"]).default("development"),
-    PORT: z.string().default("3000"),
+    NODE_ENV: z.enum(["development", "production", "test"]),
+    PORT: z.string().transform(Number).pipe(z.number().min(1).max(65535)),
 });
 
 const _env = envSchema.safeParse(process.env);
 
 if (_env.success === false) {
-    throw new AppError(400, "Erro de validação nas variáveis de ambiente");
+    console.error("Erro na validação das váriaveis de ambiente", _env.error);
+    process.exit(1);
 }
 
 export const env = _env.data;
