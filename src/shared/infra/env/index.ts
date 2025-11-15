280a0b2ade28f2d6e6632b6157f777b3099dbac2
Italo <italoteles399@gmail.com>
2025-11-15 08:46:35 -0300

    adicionado validações nas variáveis de ambientes database

--- src/shared/infra/env/index.ts
+++ src/shared/infra/env/index.ts
@@ -5,6 +5,13 @@ dotenv.config();
 const envSchema = z.object({
     NODE_ENV: z.enum(["development", "production", "test"]),
     PORT: z.string().transform(Number).pipe(z.number().min(1).max(65535)),
+    DB_TYPE: z.enum(["postgres", "mongodb"]),
+    DB_USER: z.string().trim(),
+    DB_PASS: z.string().trim(),
+    DB_NAME: z.string().trim(),
+    DB_PORT: z.coerce.number().default(5432),
+    DB_SCHEMA: z.literal("public"),
+    DB_HOST: z.string().trim(),
 });
 
 const _env = envSchema.safeParse(process.env);
