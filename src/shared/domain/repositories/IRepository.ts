75d4b22df80f78df5a3e5f95576fe53ed255b3ac
Italo <italoteles399@gmail.com>
2025-11-25 01:33:23 -0300

    adicionado contrato de repositorio genérico

---
+++ src/shared/domain/repositories/IRepository.ts
@@ -0,0 +1,27 @@
+export type SearchInput = {
+    page?: number;
+    perPage?: number;
+    sort?: string;
+    sortDir?: string;
+    filter?: string;
+};
+
+export type SearchOutput<Model> = {
+    itens: Model[];
+    perPage: number;
+    total: number;
+    currentPage: number;
+    sort: string;
+    sortDir: string;
+    filter: string;
+};
+
+export interface IRepository<Model, CreateProps> {
+    // cria uma instância de um modelo de dados
+    create(props: CreateProps): Model;
+    save(model: Model): Promise<Model>;
+    findById(id: string): Promise<Model>;
+    update(model: Model): Promise<Model>;
+    delete(id: string): Promise<void>;
+    search(props: SearchInput): Promise<SearchOutput<Model>>;
+}
