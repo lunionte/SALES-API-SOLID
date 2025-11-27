a34a1e9af734c58a887408491ae1de0107e4f111
Italo <italoteles399@gmail.com>
2025-11-27 03:15:12 -0300

    adicionado lógica de busca com paginação, ordenação e filtragem na classe InMemoryRepository

--- src/shared/domain/repositories/In-memory.repository.ts
+++ src/shared/domain/repositories/In-memory.repository.ts
@@ -16,6 +16,7 @@ export type CreateProps = {
 export abstract class InMemoryRepository implements IRepository<Model, CreateProps> {
     items: Model[] = [];
 
+    // createdAt, name etc
     sortableFields: string[] = [];
 
     // POR SER REPO EM MEMÓRIA, SEMPRE LANÇAR ERRORS (NORMALMENTE USADO EM TESTS)
@@ -62,7 +63,61 @@ export abstract class InMemoryRepository implements IRepository<Model, CreatePro
         const index = this.items.findIndex((i) => i.id === id);
         this.items.splice(index, 1);
     }
-    search(props: SearchInput): Promise<SearchOutput<Model>> {
-        throw new Error("Method not implemented.");
+
+    async search(props: SearchInput): Promise<SearchOutput<Model>> {
+        const page = props.page ?? 1;
+        const perPage = props.perPage ?? 15;
+        const sort = props.sort ?? null;
+        const sortDir = props.sortDir ?? null;
+        const filter = props.filter ?? null;
+
+        const filteredItems = await this.applyFilter(this.items, filter);
+        const ordernedItems = await this.applySort(filteredItems, sort, sortDir);
+        const paginatedItems = await this.applyPaginate(ordernedItems, page, perPage);
+
+        return {
+            items: paginatedItems,
+            total: filteredItems.length,
+            currentPage: page,
+            perPage,
+            sort,
+            sortDir,
+            filter,
+        };
+    }
+
+    protected abstract applyFilter(items: Model[], filter: string | null): Promise<Model[]>;
+
+    protected async applySort(items: Model[], sort: string | null, sortDir: string | null): Promise<Model[]> {
+        if (!sort || !this.sortableFields.includes(sort)) {
+            return items;
+        }
+
+        // -1 : o A vem ANTES de B
+        // +1 : o A vem DEPOIS de B
+        return [...items].sort((a, b) => {
+            // mesma coisa que por ex: a.age, b.age
+            // if (sortDir === "asc") {
+            //     if (a[sort] < b[sort]) return -1;
+
+            //     // 35       // 22
+            //     if (a[sort] > b[sort]) return 1;
+            // }
+            // if (sortDir === "desc") {
+            //     if (a[sort] < b[sort]) return 1;
+            //     if (a[sort] > b[sort]) return -1;
+            // }
+
+            if (a[sort] < b[sort]) return sortDir === "asc" ? -1 : 1;
+            if (a[sort] > b[sort]) return sortDir === "asc" ? 1 : -1;
+
+            return 0;
+        });
+    }
+
+    protected async applyPaginate(items: Model[], page: number, perPage: number): Promise<Model[]> {
+        const start = (page - 1) * perPage;
+        const end = start + perPage;
+        return items.slice(start, end);
     }
 }
